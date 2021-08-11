45aa2c3b5f221413995963430235244bd9597325
Serg Nikolaev <kndnou@yandex.ru>
2021-08-11 16:19:27 +1100

    app rewrited with OOP and some cleans

--- screen.py
+++ screen.py
@@ -2,19 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import pygame
-import random
+from random import random
 
 SCREEN_DIM = (800, 600)
 
 
 class Vec2d():
-    """Реализовать класс 2-мерных векторов Vec2d [1]. В классе следует определить методы для основных математических
+    """Реализовать класс 2-мерных векторов Vec2d. В классе следует определить методы для основных математических
     операций, необходимых для работы с вектором: Vec2d.__add__ (сумма), Vec2d.__sub__ (разность),
     Vec2d.__mul__ (произведение на число). А также добавить возможность вычислять длину вектора с использованием
     функции len(a) и метод int_pair, который возвращает кортеж из двух целых чисел (текущие координаты вектора).
-
-    [1] Вектор определяется координатами x, y — точка конца вектора.
-    Начало вектора всегда совпадает с центом координат (0, 0).
     """
     def __init__(self, x, y, speed_x=0, speed_y=0):
         self.x, self.y = x, y
@@ -22,29 +19,22 @@ class Vec2d():
 
     def __add__(self, other):
         """возвращает сумму двух векторов"""
-        return Vec2d(
-            self.x + other.x, self.y + other.y,
-            self.speed_x + other.speed_x, self.speed_y + other.speed_y
-        )
+        return Vec2d(self.x + other.x, self.y + other.y,
+                     self.speed_x + other.speed_x, self.speed_y + other.speed_y)
 
     def __sub__(self, other):
         """возвращает разность двух векторов"""
-        return Vec2d(
-            self.x - other.x, self.y - other.y,
-            self.speed_x - other.speed_x, self.speed_y - other.speed_y
-        )
+        return Vec2d(self.x - other.x, self.y - other.y,
+                     self.speed_x - other.speed_x, self.speed_y - other.speed_y)
 
     def __mul__(self, k):
         """возвращает произведение вектора на число"""
-        return Vec2d(
-            self.x * k, self.y * k,
-            self.speed_x * k, self.speed_y * k
-        )
+        return Vec2d(self.x*k, self.y*k,
+                     self.speed_x*k, self.speed_y*k)
 
     def __len__(self):
         """Реализация весьма экзотического запроса от заказчика"""
-        a = self.get_A()
-        b = self.get_B()
+        a, b = self.get_A(), self.get_B()
         ab = ((b[0] - a[0])**2 + (b[1] - a[1])**2) ** 0.5
         return int(ab)
 
@@ -83,13 +73,11 @@ class Polyline():
 
     def add_point(self, x, y, speed_x, speed_y):
         self.vectors.append(Vec2d(x, y, speed_x, speed_y))
-        # self.speeds.append(Vec2d(speed_x, speed_y))
 
     def set_points(self):
         """функция перерасчета координат опорных точек"""
         vectors = self.vectors
         screen_dim = self.screen_dim
-        # points, speeds = self.points, self.speeds
         for p in range(len(vectors)):
             vectors[p] = vectors[p] + vectors[p].get_speed()
             if vectors[p].x > screen_dim[0] or vectors[p].x < 0:
@@ -108,14 +96,7 @@ class Polyline():
 
 class Knot(Polyline):
     """Реализовать класс Knot (наследник класса Polyline), в котором добавление и пересчёт координат инициируют вызов
-    функции get_knot для расчёта точек кривой по добавляемым «опорным» точкам [2].
-
-    [2] Здесь стоит уточнить, что стоит различать понятия точек, используемых в описании задания.
-    Существуют два вида: «опорные» и «сглаживания». Первые задают положение углов замкнутой ломаной и служат основой
-    для вычисления вторых. Количество точек «сглаживания» определяет насколько плавными будут обводы углов ломаной.
-    Вы можете поэкспериментировать с изменением количества точек сглаживания (см. команды программы) и понаблюдать,
-    как изменяется отрисовка линии при различных значениях (текущее количество точек «сглаживания» можно посмотреть
-    на экране справки).
+    функции get_knot для расчёта точек кривой по добавляемым «опорным» точкам.
     """
     def get_point(self, base_points, alpha, deg=None):
         if deg is None:
@@ -158,13 +139,12 @@ class Knot(Polyline):
 
 
 class Game:
-    def __init__(self, steps=10, working=True, show_help=False, pause=True,
-                 hue=0, color=pygame.Color(0), caption="MyScreenSaver", screen_dim=(800, 600)):
+    def __init__(self, steps=10, color=pygame.Color(0), caption="MyScreenSaver", screen_dim=(800, 600)):
         self.steps = steps
-        self.working = working
-        self.show_help = show_help
-        self.pause = pause
-        self.hue = hue
+        self.show_help = False
+        self.pause = True
+        self.show_closed_curve = True
+        self.hue = 0
         self.color = color
         self.gameDisplay = pygame.display.set_mode(screen_dim)
         self.a_polyline = Knot(self.gameDisplay, screen_dim)
@@ -172,17 +152,9 @@ class Game:
         self.screen_dim = screen_dim
 
     def start(self):
-        working = self.working
-        show_help = self.show_help
-        pause = self.pause
-        hue = self.hue
-        color = self.color
-        a_polyline = self.a_polyline
-        screen_dim = self.screen_dim
-        caption = self.caption
-        show_closed_curve = True
         pygame.init()
-        pygame.display.set_caption(caption)
+        pygame.display.set_caption(self.caption)
+        working = True
         while working:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -191,59 +163,43 @@ class Game:
                     if event.key == pygame.K_ESCAPE:
                         working = False
                     if event.key == pygame.K_r:
-                        a_polyline.reset()
+                        self.a_polyline.reset()
                     if event.key == pygame.K_p:
-                        pause = not pause
+                        self.pause = not self.pause
                     if event.key == pygame.K_h:  # Show or hide closed curve
-                        show_closed_curve = not show_closed_curve
+                        self.show_closed_curve = not self.show_closed_curve
                     if event.key == pygame.K_KP_PLUS:
-                        self.set_steps_up(1)
+                        self.set_less_basepoints(1)
                     if event.key == pygame.K_UP:
-                        self.set_steps_up(1)
+                        self.set_less_basepoints(1)
                     if event.key == pygame.K_F1:
-                        show_help = not show_help
+                        self.show_help = not self.show_help
                     if event.key == pygame.K_KP_MINUS:
                         self.set_steps_down(1)
                     if event.key == pygame.K_DOWN:
                         self.set_steps_down(1)
-
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    speed_mod = 0.5
-                    a_polyline.add_point(
-                        event.pos[0], event.pos[1], random.random() * speed_mod, random.random() * speed_mod
-                    )
-
-            # TODO put it in next_step()
-            self.gameDisplay.fill((0, 0, 0))
-            hue = (hue + 1) % 360
-            color.hsla = (hue, 100, 50, 100)
-            a_polyline.draw_points()
-            if show_closed_curve:
-                a_polyline.get_knot(self.steps, 3, color)
-            if not pause:
-                a_polyline.set_points()
-            if show_help:
-                self.draw_help()
-            pygame.display.flip()
-            # TODO ^ ^ ^
+                    speed = 0.5
+                    self.a_polyline.add_point(event.pos[0], event.pos[1],
+                                              random()*speed, random()*speed)
+            self.next_step()
         pygame.display.quit()
         pygame.quit()
 
     def next_step(self):
-        pass
-        # self.gameDisplay.fill((0, 0, 0))
-        # hue = (hue + 1) % 360
-        # color.hsla = (hue, 100, 50, 100)
-        # a_polyline.draw_points()
-        # if show_closed_curve:
-        #     a_polyline.get_knot(self.steps, 3, color)
-        # if not pause:
-        #     a_polyline.set_points()
-        # if show_help:
-        #     self.draw_help()
-        # pygame.display.flip()
+        self.gameDisplay.fill((0, 0, 0))
+        self.hue = (self.hue + 1) % 360
+        self.color.hsla = (self.hue, 100, 50, 100)
+        self.a_polyline.draw_points()
+        if self.show_closed_curve:
+            self.a_polyline.get_knot(self.steps, 3, self.color)
+        if not self.pause:
+            self.a_polyline.set_points()
+        if self.show_help:
+            self.draw_help()
+        pygame.display.flip()
 
-    def set_steps_up(self, steps):
+    def set_less_basepoints(self, steps):
         self.steps += steps
 
     def set_steps_down(self, steps):
@@ -262,11 +218,13 @@ class Game:
         data.append(["F1", "Show Help"])
         data.append(["R", "Restart"])
         data.append(["P", "Pause/Play"])
+        data.append(["H", "Hide/Show curve"])
         data.append(["Num+", "More points"])
+        data.append(["UP", "More points"])
         data.append(["Num-", "Less points"])
+        data.append(["DOWN", "Less points"])
         data.append(["", ""])
         data.append([str(steps), "Current points"])
-
         pygame.draw.lines(self.gameDisplay, (255, 50, 50, 255), True, [
             (0, 0), (800, 0), (800, 600), (0, 600)], 5)
         for i, text in enumerate(data):
